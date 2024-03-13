6a7f0f6511eadfe29e925bcbb11beca556f319a1
Isadora Oliveira <isadora.mendonca@mercadolivre.com>
2024-03-12 22:06:52 -0300

    Initial commit with Spring Boot project structure

---
+++ src/main/java/com/clasichotel/ClasicHotelApplication.java
@@ -0,0 +1,13 @@
+package com.clasichotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasicHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasicHotelApplication.class, args);
+	}
+
+}
